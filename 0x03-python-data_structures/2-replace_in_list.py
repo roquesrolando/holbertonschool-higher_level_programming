94a0f8c3303d06468400656db27037e7e17c5c16
roquesrolando <roquesrolando@hotmail.com>
2021-05-04 16:21:35 +0000

    pep8 error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,5 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif idx <= len(my_list) - 1:
         my_list[idx] = element
         return my_list
-    elif idx > len(my_list) -1 :
+    elif idx > len(my_list) -1:
         return my_list
