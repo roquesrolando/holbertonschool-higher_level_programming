321e6274abc07bcf241a1b2c0c8aeb05f2fc971d
roquesrolando <roquesrolando@hotmail.com>
2021-05-04 16:13:44 +0000

    fix if order

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx <= len(my_list):
-        return (my_list[idx])
-    elif idx < 0:
-        return None
+    if idx < 0:
+        return
+    elif idx <= len(my_list):
+        return my_list[idx]
     elif idx > len(my_list):
-        return None
+        return
