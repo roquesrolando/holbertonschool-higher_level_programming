b54f7d729c408dd40e4bafcfc439d3b927a53db2
roquesrolando <roquesrolando@hotmail.com>
2021-05-04 17:07:25 +0000

    fix code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence is not None:
+    if length != 0:
         return length, sentence[0]
     return length, None
