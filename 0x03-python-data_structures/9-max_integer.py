4bd43ec78ec6595cdf0ccf203bf92d4f05c3c929
roquesrolando <roquesrolando@hotmail.com>
2021-05-04 17:27:17 +0000

    edge cases

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    new = my_list[0]
     if len(my_list) == 0:
         return None
+    new = my_list[0]
     for i in my_list:
         if i > new:
             new = i
