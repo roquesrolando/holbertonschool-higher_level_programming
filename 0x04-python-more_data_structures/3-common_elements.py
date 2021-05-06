ba370b81c50f46d625fe4ad2404b08ef217148f3
roquesrolando <roquesrolando@hotmail.com>
2021-05-06 16:40:57 +0000

    update

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 is not None or ser_2 is not None:
-        elements = []
-        for i in set_1:
-            for j in set_2:
-                if i == j:
-                    elements += j
-        return set(elements)
+    return set_1 & set_2
