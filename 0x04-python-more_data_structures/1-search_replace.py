bf1760a3e87864bba47928670421262b207cd35b
roquesrolando <roquesrolando@hotmail.com>
2021-05-06 16:34:43 +0000

    read the instructions better

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is not None:
-        new = my_list.copy()
-        for i in range(len(my_list)):
-            if search == 0:
-                new[0] = replace
-            elif i == search - 1:
-                new[i] = replace
-        return new
+    new = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new[i] = replace
+    return new
