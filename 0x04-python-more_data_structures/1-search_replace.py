f4da405de4c956ea13469ba96ae13781edd14bf2
roquesrolando <roquesrolando@hotmail.com>
2021-05-06 16:05:08 +0000

    edge cases

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = my_list.copy()
-    for i in my_list:
-        if i == search - 1:
-            new[i] = replace
-    return new
+    if my_list is not None:
+        new = my_list.copy()
+        for i in my_list:
+            if i == search - 1:
+                new[i] = replace
+        return new
