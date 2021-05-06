c75ae86906b0710c56258791b5369a80c5c9e584
roquesrolando <roquesrolando@hotmail.com>
2021-05-06 18:28:23 +0000

    fix pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return 0
     value = 0
-    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, 
+    roman = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
             "D": 500, "M": 1000}
     for i in roman_string:
         for j, z in roman.items():
