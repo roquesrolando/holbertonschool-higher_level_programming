fed987e97ff7414e99efe81813a5a0ec473ff655
roquesrolando <roquesrolando@hotmail.com>
2021-05-18 20:50:37 +0000

    edge cases

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,9 @@ class Square:
     '''This clas defines the Square'''
     def __init__(self, size=0):
         '''This methods creates a new instance of square'''
-        try:
+        if type(size) is int:
             self.__size = size
             if size < 0:
                 raise ValueError("size must be >= 0")
-        except TypeError:
+        else:
             raise TypeError("size must be a integer")
