1066265ed6f55b75d63a5050d8e2c99f0c2ddaf4
roquesrolando <roquesrolando@hotmail.com>
2021-05-18 21:08:46 +0000

    pep8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,7 +20,7 @@ class Square:
         '''Setter method for size'''
         if type(value) is int:
             if value < 0:
-                 raise ValueError("size must be >= 0")
+                raise ValueError("size must be >= 0")
             self.__size = value
         else:
             raise TypeError("size must be an integer")
