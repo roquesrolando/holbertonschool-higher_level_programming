a2fd5f333a473b1059b1cf95b8ce275625bd6e16
roquesrolando <roquesrolando@hotmail.com>
2021-06-02 19:37:43 +0000

    first commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class of a list"""
+
+
+class MyList(list):
+    """A class from list'"""
+    def print_sorted(self):
+
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+
+    if type(obj) is not a_class:
+        return False
+    return True
