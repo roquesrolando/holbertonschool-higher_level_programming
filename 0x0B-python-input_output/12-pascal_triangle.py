d2fb4e01ee75ee854c05ac92626090c56f6159b1
roquesrolando <roquesrolando@hotmail.com>
2021-06-04 16:23:00 +0000

    first commit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''returns a list of pascal triangle'''
+
+
+def pascal_triangle(n):
+    '''returns a list of pascal triangle'''
+
+    lists = [[1]]
+
+    for x in range(1, n):
+        pascal = []
+        for i in range(x + 1):
+            if i == 0:
+                pascal.append(1)
+                continue
+            if i == x:
+                pascal.append(1)
+                continue
+            pascal.append(lists[x - 1][i - 1] + lists[x - 1][i])
+        lists.append(pascal)
+    return lists
