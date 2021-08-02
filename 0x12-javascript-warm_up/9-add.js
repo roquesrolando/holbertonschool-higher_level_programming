429f4bf8b16eddd7544c64d44576fc623b9f4022
roquesrolando <roquesrolando@hotmail.com>
2021-08-02 17:51:14 +0000

    first commit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const number1 = parseInt(process.argv[2]);
+const number2 = parseInt(process.argv[3]);
+
+if (number1 && number2) {
+  console.log(number1 + number2);
+} else {
+  console.log(number1 + number2);
+}
