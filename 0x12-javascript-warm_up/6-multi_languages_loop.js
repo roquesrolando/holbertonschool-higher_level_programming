3bfd87349cd16fcf1588c8c72213d88d7460ca0c
roquesrolando <roquesrolando@hotmail.com>
2021-08-02 17:29:02 +0000

    first commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const elements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const count = elements.length - 1;
+
+for (let i = 0; i <= count; i++) {
+  console.log(elements[i]);
+}
