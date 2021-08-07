53280842142a06322b74e6b592988a85bb82a096
roquesrolando <roquesrolando@hotmail.com>
2021-08-07 17:37:09 +0000

    added back

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""List all states from the database"""
+import sys
+import MySQLdb
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+database = sys.argv[3]
+
+db = MySQLdb.connect('localhost', user, passwd, database, 3306)
+
+c = db.cursor()
+c.execute("SELECT * FROM states ORDER BY id ASC")
+rows = c.fetchall()
+for row in rows:
+    print(row)
+
+db.close()
