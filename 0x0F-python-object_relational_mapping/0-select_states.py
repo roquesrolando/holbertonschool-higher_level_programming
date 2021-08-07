36f0510d70b1f4abdf3ba177e2a5a7cf02f767b2
roquesrolando <roquesrolando@hotmail.com>
2021-08-07 17:00:19 +0000

    fix lenght

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """List all states from the database"""
 
-
 import sys
 import MySQLdb
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-database = sys.argv[3]
+user, passwd, database = sys.argv[1:4]
 db = MySQLdb.connect('localhost', user, passwd, database, 3306)
 c = db.cursor()
 c.execute("SELECT * FROM states ORDER BY id ASC")
