2abc9fe3a88958bef27d594b4d19e51fd2d1b655
roquesrolando <roquesrolando@hotmail.com>
2021-08-08 18:31:18 +0000

    edge case

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,4 +16,7 @@ if __name__ == "__main__":
     session = Session()
 
     states = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(states.id, states.name))
+    if states is None:
+        print('Nothing')
+    else:
+        print("{}: {}".format(states.id, states.name))
