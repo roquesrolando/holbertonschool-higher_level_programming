35679524579bf01519f3a5a67503d19c064f534f
roquesrolando <roquesrolando@hotmail.com>
2021-08-08 19:01:51 +0000

    first upload

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module lists State objects"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(user, passwd, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(text("name='{}'".format(name)))
+    for instance in states:
+        print("{}".format(instance.id))
