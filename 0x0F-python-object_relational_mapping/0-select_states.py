92a1e834184b355d3071f34dc6da1ccb67c06dca
roquesrolando <roquesrolando@hotmail.com>
2021-08-08 16:34:52 +0000

    update try and except

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,10 +10,13 @@ if __name__  == "__main__":
 
     db = MySQLdb.connect('localhost', user, passwd, database, 3306)
 
-    c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
+    try:
+        c = db.cursor()
+        c.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = c.fetchall()
+        for row in rows:
+            print(row)
+    except:
+        db.rollback()
 
     db.close()
