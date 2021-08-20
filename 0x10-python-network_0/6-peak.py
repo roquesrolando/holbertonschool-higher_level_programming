6ced75c103aeeb0134b49789a70d77c9edd93e72
roquesrolando <roquesrolando@hotmail.com>
2021-08-20 16:01:25 +0000

    fix edge case

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Find a Peak"""
+"""Finds a peak"""
 
 
 def find_peak(list_of_integers):
     """Finds a peak"""
 
+    if type(list_of_integers) is not list:
+        return None
     length = len(list_of_integers)
     if length == 0:
         return None
     if length == 1:
         return l[1]
+    if list_of_integers[0] > list_of_integers[1]:
+        return list_of_integers[0]
+    if list_of_integers[-1] > list_of_integers[-2]:
+        return list_of_integers[-1]
 
     for idx in range(1, length):
         if (list_of_integers[idx - 1] < list_of_integers[idx] and
