77106eea8d3e4a731d87a25c543e373054e26f7b
roquesrolando <roquesrolando@hotmail.com>
2021-08-20 16:18:51 +0000

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    web = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"\
+            .format(type(web), web, web.decode('utf8')))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+# here are all the answers for 0x11-python-network_1
