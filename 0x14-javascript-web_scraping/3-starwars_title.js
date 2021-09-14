3deda92d24379946f40ba60f2ad90a65d8ebe451
roquesrolando <roquesrolando@hotmail.com>
2021-09-14 22:34:40 +0000

    upload task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const starwars = 'https://swapi-api.hbtn.io/api/films/'
+request(starwars + process.argv[2], (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+	body = JSON.parse(body);
+	console.log(body.title);
+});
