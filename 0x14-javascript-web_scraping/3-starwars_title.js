9c448177bccc1e7618074cdc436dfdd0a6ae9a47
roquesrolando <roquesrolando@hotmail.com>
2021-09-14 22:36:34 +0000

    semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 
-const starwars = 'https://swapi-api.hbtn.io/api/films/'
+const starwars = 'https://swapi-api.hbtn.io/api/films/';
 request(starwars + process.argv[2], (err, res, body) => {
   if (err) {
     console.error(err);
     return;
   }
-	body = JSON.parse(body);
-	console.log(body.title);
+  body = JSON.parse(body);
+  console.log(body.title);
 });
