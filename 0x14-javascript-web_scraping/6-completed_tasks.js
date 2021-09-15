d6fb9af7335d63edc36ec2c8be694de7b8fee2a7
roquesrolando <roquesrolando@hotmail.com>
2021-09-15 00:12:12 +0000

    uploaded task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const todo = JSON.parse(body);
+  const dict = {};
+  let user = 1;
+  let count = 0;
+  for (const task of todo) {
+    if (task.userId !== user) {
+      user++;
+      count = 0;
+    }
+    if (task.completed) {
+      count++;
+      dict[task.userId] = count;
+    }
+  }
+  console.log(dict);
+});
