90006759d3b88e8f12c9a39fdb8541b016e312a4
roquesrolando <roquesrolando@hotmail.com>
2021-09-15 00:18:16 +0000

    trying

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,7 +8,7 @@ request(url, (err, res, body) => {
     return;
   }
   const todo = JSON.parse(body);
-  const dict = {};
+  let dict = {};
   let user = 1;
   let count = 0;
   for (const task of todo) {
